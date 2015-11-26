2dd1190d44bde11bf65599284dcaee363f6bfd37
dandelea <dandelea@alum.us.es>
2015-11-26 13:28:32 +0000

    Test e2e

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
-		//'e2e/TC01-getContacts.js',
+		'e2e/TC01-getProducts.js',
 		'backend/TC01-getProducts.js',
 	]
 };
