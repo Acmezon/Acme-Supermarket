553139653d853dbdcefe59194684a1749027f205
alesanmed <alesanmed@alum.us.es>
2015-11-26 13:18:11 +0100

    Creación de un test de backend

---
+++ test/backend/TC01-getProducts.js
@@ -0,0 +1,15 @@
+var request = require('supertest');
+var should = require('should');
+var assert = require('assert');
+
+describe("Get products from the API", function (){
+	it("get products from the Products api", function(){
+
+		request("http://localhost:3000")
+			.get("/api/products")
+			.end(function(err, res){
+				res.body.length.should.be.equal(2);
+				res.status.should.be.equal(200);
+			});
+	});
+});
