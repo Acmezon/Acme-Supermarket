0dbb780e1dea525217428291c16afe5c82e8781f
dandelea <dandelea@alum.us.es>
2015-11-27 00:23:16 +0000

    Validación en acceso de usuario

--- public/views/login/LoginController.js
+++ public/views/login/LoginController.js
@@ -1,5 +1,12 @@
 'use strict'
 
 angular.module('acme_supermarket').registerCtrl('LoginCtrl', ['$scope', '$http', function ($scope, $http) {
+
+	// Function invoked by login submit
+	$scope.submitLogin = function() {
+
+		
+
+	}
 	
 }]);
