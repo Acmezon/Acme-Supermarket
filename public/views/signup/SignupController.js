294769fb17a0d286d704ba80d48af3f33dc74fe9
dandelea <dandelea@alum.us.es>
2015-11-27 13:07:40 +0000

    Validacion Signup

--- public/views/signup/SignupController.js
+++ public/views/signup/SignupController.js
@@ -2,4 +2,11 @@
 
 angular.module('acme_supermarket').registerCtrl('SignupCtrl', ['$scope', '$http', function ($scope, $http) {
 	
+	// Function invoked by login submit
+	$scope.submitSignup = function() {
+
+		
+
+	}
+
 }]);
