8adfe06eed01ea52702f7ca04c13a17e68204a56
Ale Sánchez <alesanmed@alum.us.es>
2015-12-02 12:17:08 +0100

    Cambiada la ruta de los tests para que coja todos los de e2e y backend sin distinción.

--- test/test.js
+++ test/test.js
@@ -3,13 +3,8 @@ var request = require('supertest');
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
-		//'e2e/products/TC01-getProducts.js',
-		'backend/products/TC01-getProducts.js',
-		'backend/authentification/TC02-signup.js',
-		'backend/authentification/TC03-signup-existingEmail.js',
-		'backend/authentification/TC04-signup-invalidField.js',
-		'backend/authentification/TC05-signup-missingField.js',
-		'backend/authentification/TC06-signin.js'
+		'e2e/**',
+		'backend/**'
 	],
 	beforeLaunch: function() {
 		request("http://localhost:3000")
