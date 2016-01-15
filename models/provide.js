d567c740cba80a9ed16c628f3738f153e9b43195
dandelea <dandelea@alum.us.es>
2016-01-15 17:34:18 +0100

    Models B provide and products

---
+++ models/provide.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+
+var provideSchema = mongoose.Schema({
+	price: {type: Number, required:true, min:0, get: getPrice, set:setPrice},
+	isEnabled : {type: Boolean, required:true, default: true}
+	product_id : {type: mongoose.Schema.Types.ObjectId, ref:'productSchema'},
+	supplier_id : {type: mongoose.Schema.Types.ObjectId, ref:'supplierSchema'}
+});
+
+// Store price as cents. Avoid rare storage
+function getPrice(num){
+    return (num/100).toFixed(2);
+}
+
+function setPrice(num){
+    return num*100;
+}
+
+
+module.exports = mongoose.model('Provide', provideSchema);
