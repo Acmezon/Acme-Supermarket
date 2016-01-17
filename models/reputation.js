93ad2bac8e83b6cf830a2c2c65d5cc6acea1b34c
Ale Sánchez <alesanchezmedina@gmail.com>
2016-01-17 11:23:15 +0100

    Corregidos fallos de los modelos y terminado el archivo de populado de la base de datos.

--- models/reputation.js
+++ models/reputation.js
@@ -1,14 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Supplier = require('./supplier'),
-	Customer = require('./customer');
+	Customer = require('./customer'),
+	Purchase = require('./purchase');
 
 
 
 var reputationSchema = mongoose.Schema({
 	reputation: {type:Number,min:0,max:5, required: true},
-	supplier_id: {type: mongoose.Schema.Types.ObjectId, ref:'Supplier'},,
-	customer_id: {type: mongoose.Schema.Types.ObjectId, ref:'Customer'}
+	supplier_id: {type: mongoose.Schema.Types.ObjectId, ref:'Supplier'},
+	customer_id: {type: mongoose.Schema.Types.ObjectId, ref:'Customer'},
+	purchase_id: {type: mongoose.Schema.Types.ObjectId, ref:'Purchase'}
 });
 
 
