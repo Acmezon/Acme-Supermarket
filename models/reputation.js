8d4a66ef412c433fac6afa9283fc7e3de3bdf22b
Ale Sánchez <alesanchezmedina@gmail.com>
2016-01-17 11:31:56 +0100

    Modificados modelos y archivo de poblado.

--- models/reputation.js
+++ models/reputation.js
@@ -1,16 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Supplier = require('./supplier'),
-	Customer = require('./customer'),
-	Purchase = require('./purchase');
+	Customer = require('./customer');
 
 
 
 var reputationSchema = mongoose.Schema({
 	reputation: {type:Number,min:0,max:5, required: true},
 	supplier_id: {type: mongoose.Schema.Types.ObjectId, ref:'Supplier'},
-	customer_id: {type: mongoose.Schema.Types.ObjectId, ref:'Customer'},
-	purchase_id: {type: mongoose.Schema.Types.ObjectId, ref:'Purchase'}
+	customer_id: {type: mongoose.Schema.Types.ObjectId, ref:'Customer'}
 });
 
 
