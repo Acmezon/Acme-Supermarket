aff682a61b341d075675dba9c673330731eed2d0
dandelea <dandelea@alum.us.es>
2016-01-27 12:47:06 +0100

    BUGFIX: Lista de mis provides

--- routes/db_utils.js
+++ routes/db_utils.js
@@ -49,3 +49,35 @@ exports.handleInsertErrors = function(err){
 	}
 	return errors;
 };
+
+/* finds the intersection of 
+ * two arrays in a simple fashion.  
+ *
+ * PARAMS
+ *  a - first array, must already be sorted
+ *  b - second array, must already be sorted
+ *
+ * NOTES
+ *
+ *  Should have O(n) operations, where n is 
+ *    n = MIN(a.length(), b.length())
+ */
+exports.intersect_safe = function(a, b)
+{
+  var ai=0, bi=0;
+  var result = new Array();
+
+  while( ai < a.length && bi < b.length )
+  {
+     if      (a[ai] < b[bi] ){ ai++; }
+     else if (a[ai] > b[bi] ){ bi++; }
+     else /* they're equal */
+     {
+       result.push(a[ai]);
+       ai++;
+       bi++;
+     }
+  }
+
+  return result;
+}
