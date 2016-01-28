903b1dee2893aa75dcdf1e9ebd6ab673e7b6fd38
dandelea <dandelea@alum.us.es>
2016-01-28 11:18:59 +0100

    Test e2e signout

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ exports.config = {
 	},
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
-		'e2e/products/TC03-productEdition.js'
+		'e2e/authentication/TC03-signout.js'
 		//'backend/products/TC03-rateProduct.js'
 
 	]/*,
