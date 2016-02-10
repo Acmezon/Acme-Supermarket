d5d8518398b4a14d31fb0243e41818492a897570
Ale Sánchez <alesanchezmedina@gmail.com>
2016-02-10 10:41:44 +0100

    BUGFIX: Reglas creadas en el día después de la hora de ejecución no se ejecutaban nunca

--- routes/scheduled_tasks.js
+++ routes/scheduled_tasks.js
@@ -5,15 +5,12 @@ var schedule = require('node-schedule'),
 	log = require('../logger');
 
 exports.scheduleAutomaticPurchases = function() {
-											//Every day at 17:00
-	var autoPurchase = schedule.scheduleJob({hour: 17, minute: 17}, function(){
+											//Every day at 03:00AM
+	var autoPurchase = schedule.scheduleJob({hour: 3, minute: 0}, function(){
 		var start = new Date();
 		start.setHours(0,0,0,0);
 
-		var end = new Date();
-		end.setHours(23,59,59,999);
-
-		PurchasingRule.find({ nextRun : { $gte: start, $lt: end} }, function (err, results) {
+		PurchasingRule.find({ nextRun : { $lt: end } }, function (err, results) {
 			if(err) {
 				console.log(err);
 			}
@@ -24,8 +21,6 @@ exports.scheduleAutomaticPurchases = function() {
 					var rule = results[i];
 
 					var result = sync.await(PurchaseService.purchaseScheduled(rule.customer_id, rule.provide_id, rule.quantity, sync.defer()));
-
-					console.log("Purchased: " + result);
 
 					var nextRun = new Date(rule.nextRun);
 
